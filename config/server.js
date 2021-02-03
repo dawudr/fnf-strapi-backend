f086cf607c57b2b62d02286e1e5ad31cd9ca865e
Dawud Rahman <dawudr@gnmail.com>
2021-02-03 13:20:50 +0000

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('HEROKU_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '143def00dacbdf7e8c39572441bff45f'),
+    },
+  },
 });
